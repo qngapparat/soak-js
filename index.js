a632b265ec78c69a9e6d8528c2fc592c91d4a7cb
qngapparat <jakob.wallnoefer@outlook.com>
2020-01-07 20:20:15 +0100

    init Platform detection

--- index.js
+++ index.js
@@ -2,7 +2,30 @@
 const amazon = require('./amazon');
 const google = require('./google');
 
+function getExecutingPlatform(first = {}, second = {}, ...rest = []) {
+  // get platform
+  // on amazon, second param is 'context' and hsa these fields
+  if (
+    typeof second.invokedFunctionArn === 'string'
+    && typeof second.functionName === 'string'
+    && typeof second.awsRequestId === 'string'
+  ) {
+    return 'amazon'
+  }
+
+  if(
+    typeof first.method === 'string'
+    && first.get  // some methods that Express' Request objs alwys have
+    && first.accepts 
+    && first.acceptsCharsets
+    && first.param
+  ) {
+    return 'google'
+  }
+
+  throw new Error("Couldn't recognize platform running this code, seems neither to be GC Functions nor Amazon Lambda");
+}
+
 module.exports = {
-  amazon,
-  google
+  getExecutingPlatform
 }
---
+++ utils.js
@@ -0,0 +1,12 @@
+/**
+ * @returns {('google'|'amazon')} The platform this code is executing on 
+ */
+
+function getExecutingPlatform() {
+
+}
+
+
+module.exports = {
+
+}
